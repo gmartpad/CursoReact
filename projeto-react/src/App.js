8ac34f13e9c0494a0cb00bb0438d4286b4220a7c
Gabriel Martins Padoin <gabrielmpwork@gmail.com>
2020-08-01 18:14:38 -0300

    Componentes e Props

--- projeto-react/src/App.js
+++ projeto-react/src/App.js
@@ -1,25 +1,41 @@
 import React from 'react';
 
-// Componente por Função
+// function formataNome(usuario){
+//     return usuario.nome+" "+usuario.sobrenome;
+// }
 
-function formataNome(usuario){
-    return usuario.nome+" "+usuario.sobrenome;
+// function BemVinde(props) {
+//     return <h1>Bem-vinde {props.nome}! Você tem {props.idade} anos, certo?</h1>
+// }
+
+function Avatar(props) {
+    return (
+        <>
+            <img src={props.user.url} alt={props.user.name} />
+            <br/>
+            <strong>{props.user.name}</strong>
+        </>
+    );
 }
 
 function App(){
 
-    let usuario = {
-        nome: 'Gabriel',
-        sobrenome: 'Martins Padoin'
+    let user = {
+        url: "https://www.google.com.br/google.jpg",
+        name: "logo do google"
     }
 
-    let idade = 19;
+    // let idade = 19;
 
-    let imagem = 'https://www.google.com.br/google.jpg';
+    // let imagem = 'https://www.google.com.br/google.jpg';
 
   return <>
-    <h1>meu nome é {formataNome(usuario)} e eu tenho {idade} anos</h1>
-    <img className="classe" style={{width:100, height:100}} alt="logo do google" src={imagem} />
+    <Avatar user={user}/>
+    {/* <BemVinde nome="Joana" idade="18"/>
+    <BemVinde nome="Ana" idade="38"/>
+    <BemVinde nome="Rafael" idade="27"/> */}
+    {/* <h1>meu nome é {formataNome(usuario)} e eu tenho {idade} anos</h1>
+    <img className="classe" style={{width:100, height:100}} alt="logo do google" src={imagem} /> */}
   </>;
 }
 
