0cddef1da965124daca5dd2a29926db927042972
Gabriel Martins Padoin <gabrielmpwork@gmail.com>
2020-08-01 17:43:56 -0300

    Entendendo o JSX

--- projeto-react/src/App.js
+++ projeto-react/src/App.js
@@ -1,5 +1,28 @@
 import React from 'react';
 
+// Componente por Função
+
+function formataNome(usuario){
+    return usuario.nome+" "+usuario.sobrenome;
+}
+
+function App(){
+
+    let usuario = {
+        nome: 'Gabriel',
+        sobrenome: 'Martins Padoin'
+    }
+
+    let idade = 19;
+
+    let imagem = 'https://www.google.com.br/google.jpg';
+
+  return <>
+    <h1>meu nome é {formataNome(usuario)} e eu tenho {idade} anos</h1>
+    <img className="classe" style={{width:100, height:100}} alt="logo do google" src={imagem} />
+  </>;
+}
+
 //--------------------------------------------------
 
 // Componente por Classe
@@ -14,14 +37,6 @@ import React from 'react';
 
 //---------------------------------------------------
 
-// Componente por Função
-
-// function App(){
-//   return <h1>Tomanokú</h1>;
-// }
-
-//---------------------------------------------------
-
 // Componente por Arrow Function
 
 // let App = () => {
@@ -32,6 +47,8 @@ import React from 'react';
 
 // Componente por Arrow Function encurtada
 
-let App = () => <h1>Tomanokú</h1>;
+// let App = () => <h1>Tomanokú</h1>;
+
+
 
 export default App;
