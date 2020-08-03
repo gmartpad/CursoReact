a16481b5ac7e19f423505e44233c4802e06803b7
Gabriel Martins Padoin <gabrielmpwork@gmail.com>
2020-08-02 22:00:41 -0300

    useState

--- projeto-react/src/App.js
+++ projeto-react/src/App.js
@@ -1,31 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 // import './App.css';
 
-const TitleBox = styled.div`
+// const TitleBox = styled.div`
 
-    padding: 20px;
-    border-radius: 5px;
-    border: 2px solid #00f;
-    display: flex;
-    justify-content: center;
-`;
+//     padding: 20px;
+//     border-radius: 5px;
+//     border: 2px solid #00f;
+//     display: flex;
+//     justify-content: center;
+// `;
 
-const Title = styled.h1`
+// const Title = styled.h1`
 
-    font-size: 26px;
-    color: ${props => props.color || "#f0f0f0"};
-    font-weight: bold;
-    font-style: ${props => props.ativo === true ? "italic" : "normal"};
+//     font-size: 26px;
+//     color: ${props => props.color || "#f0f0f0"};
+//     font-weight: bold;
+//     font-style: ${props => props.ativo === true ? "italic" : "normal"};
 
-`;
+// `;
 
-const TitlePequeno = styled(Title)`
+// const TitlePequeno = styled(Title)`
 
-    font-size: 16px;
-    border: 1px solid black;
+//     font-size: 16px;
+//     border: 1px solid black;
 
-`;
+// `;
 
 // function formataNome(usuario){
 //     return usuario.nome+" "+usuario.sobrenome;
@@ -47,6 +47,17 @@ const TitlePequeno = styled(Title)`
 
 function App(){
 
+    const [ contagem, setContagem ] = useState(0); // valor 0 no useState é o valor inicial de contagem
+    /* variavel do state e set(nome da variavel do state)  */
+
+    const botaoAction = (sinal, modulo) => {
+      setContagem(eval(`${contagem} ${sinal} ${modulo}`));
+    };
+
+    const moduloInputValue = () => {
+      return parseInt(document.querySelector('#moduloInput').value);
+    };
+
     // let user = {
     //     url: "https://www.google.com.br/google.jpg",
     //     name: "logo do google"
@@ -58,19 +69,28 @@ function App(){
 
   return <>
 
-    <TitleBox>
+    <div>{contagem}</div>
+    <button onClick={() => botaoAction("+", moduloInputValue())}>aumentar</button>
+    <input id="moduloInput" type="text"/>
+    <button onClick={() => botaoAction("-", moduloInputValue())}>diminuir</button>
+    <button onClick={() => botaoAction("*", moduloInputValue())}>mult</button>
+    <button onClick={() => botaoAction("/", moduloInputValue())}>div</button>
+
+
+    {/* <TitleBox>
       <Title color="#F00" ativo={true}>JAAJ</Title>
       <Title color="#0F0" ativo={true}>JEEJ</Title>
       <Title color="#00F" ativo={false}>JIIJ</Title>
       <Title ativo={false}>JOOJ</Title>
       <TitlePequeno>jooj</TitlePequeno>
-    </TitleBox>
+    </TitleBox> */}
     {/* <Avatar user={user}/> */}
     {/* <BemVinde nome="Joana" idade="18"/>
     <BemVinde nome="Ana" idade="38"/>
     <BemVinde nome="Rafael" idade="27"/> */}
     {/* <h1>meu nome é {formataNome(usuario)} e eu tenho {idade} anos</h1>
     <img className="classe" style={{width:100, height:100}} alt="logo do google" src={imagem} /> */}
+  
   </>;
 }
 
