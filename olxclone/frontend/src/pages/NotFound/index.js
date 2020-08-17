b310824dd9d904d5bac616d81d4d8857d0ae466e
Gabriel Martins Padoin <gabrielmpwork@gmail.com>
2020-08-17 01:36:16 -0300

    Página 404 (OLX Clone)

---
+++ olxclone/frontend/src/pages/NotFound/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Page = () => {
+    return (
+        <>
+            <div>Página Não Encontrada</div>
+            <Link to="/">Voltar para HOME</Link>
+        </>
+    );
+}
+
+export default Page;
