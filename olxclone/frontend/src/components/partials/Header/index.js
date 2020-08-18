e35b31483e2bec4f3772dbe903dce24d1d8127cd
Gabriel Martins Padoin <gabrielmpwork@gmail.com>
2020-08-18 18:08:01 -0300

    Logout (OLX Clone)

--- olxclone/frontend/src/components/partials/Header/index.js
+++ olxclone/frontend/src/components/partials/Header/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { HeaderArea } from './styled';
 import { Link } from 'react-router-dom';
 
-import { isLogged } from '../../../helpers/AuthHandler';
+import { isLogged, doLogout } from '../../../helpers/AuthHandler';
 
 const Header = () => {
 
     //let logged = isLogged();
 
-    let logged = false; 
+    const handleLogout = () => {
+        doLogout();
+        window.location.href = '/';
+    }
 
     return (
         <HeaderArea>
@@ -29,7 +32,7 @@ const Header = () => {
                                     <Link to="/my-account">Minha Conta</Link>
                                 </li>
                                 <li>
-                                    <Link to="/logout">Sair</Link>
+                                    <button onClick={handleLogout}>Sair</button>
                                 </li>
                                 <li>
                                     <Link to="post-an-ad" className="button">Poste um anúncio</Link>
--- olxclone/frontend/src/components/partials/Header/styled.js
+++ olxclone/frontend/src/components/partials/Header/styled.js
@@ -57,7 +57,11 @@ nav {
     li {
         margin: 0 20px;
 
-        a {
+        a, button {
+            border:0;
+            background:none;
+            cursor: pointer;
+            outline:0;
             color: #000;
             font-size:14px;
             text-decoration: none;
