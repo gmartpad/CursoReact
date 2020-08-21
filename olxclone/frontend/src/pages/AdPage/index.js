6d506629757ca26225943931d41fc12c08e74731
Gabriel Martins Padoin <gabrielmpwork@gmail.com>
2020-08-20 22:57:54 -0300

    AdPage (3 - Requisição) (OLX Clone)

--- olxclone/frontend/src/pages/AdPage/index.js
+++ olxclone/frontend/src/pages/AdPage/index.js
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { PageArea, Fake } from './styled';
 import useApi from '../../helpers/OlxAPI';
 
 import { PageContainer } from '../../components/MainComponents';
+
 
 const Page = () => {
 
@@ -11,7 +12,31 @@ const Page = () => {
     const { id } = useParams();
 
     const [loading, setLoading] = useState(true);
-    const [adInfo, setAdInfo] = useState('');
+    const [adInfo, setAdInfo] = useState({});
+
+    const formatDate = (date) => {
+        let cDate = new Date(date);
+
+        console.log(cDate);
+
+        let months = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro']
+        let cDay = cDate.getDate();
+        let cMonth = cDate.getMonth();
+        let cYear = cDate.getFullYear();
+        
+        return `${cDay} de ${months[cMonth]} de ${cYear}`;
+
+    }    
+
+    useEffect(()=>{
+        const getAdInfo = async (id) => {
+            const json = await api.getAd(id, true);
+            console.log(json);
+            setAdInfo(json);
+            setLoading(false);
+        }
+        getAdInfo(id);
+    }, []);
 
     return (
         <PageContainer>
@@ -24,9 +49,21 @@ const Page = () => {
                         <div className="adInfo">
                             <div className="adName">
                                 {loading && <Fake height={20} />}
+                                {adInfo.title &&
+                                    <h2>{adInfo.title}</h2>
+                                }
+                                {adInfo.dateCreated &&
+                                
+                                    <small>Criado em {formatDate(adInfo.dateCreated)}</small>
+                                }
                             </div>
                             <div className="adDescription">
                                 {loading && <Fake height={100} />}
+                                {adInfo.description}
+                                <hr/>
+                                {adInfo.views &&
+                                    <small>Visualizações: {adInfo.views}</small>
+                                }
                             </div>
                         </div>
                     </div>
