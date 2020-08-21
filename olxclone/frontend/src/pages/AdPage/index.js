d924f6f69404d974146b2dad18682304d0d6cbac
Gabriel Martins Padoin <gabrielmpwork@gmail.com>
2020-08-20 22:21:39 -0300

    AdPage (2 - FakeLoading) (OLX Clone)

--- olxclone/frontend/src/pages/AdPage/index.js
+++ olxclone/frontend/src/pages/AdPage/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { PageArea } from './styled';
+import { PageArea, Fake } from './styled';
 import useApi from '../../helpers/OlxAPI';
 
 import { PageContainer } from '../../components/MainComponents';
@@ -19,20 +19,25 @@ const Page = () => {
                 <div className="leftSide">
                     <div className="box">
                         <div className="adImage">
-                            ...
+                            {loading && <Fake height={300} />}
                         </div>
                         <div className="adInfo">
                             <div className="adName">
-                                ...
+                                {loading && <Fake height={20} />}
                             </div>
                             <div className="adDescription">
-                                ...
+                                {loading && <Fake height={100} />}
                             </div>
                         </div>
                     </div>
                 </div>
                 <div className="rightSide">
-                    ...
+                    <div className="box box--padding">
+                        {loading && <Fake height={20} />}
+                    </div>
+                    <div className="box box--padding">
+                        {loading && <Fake height={50} />}
+                    </div>
                 </div>
             </PageArea>
         </PageContainer>
