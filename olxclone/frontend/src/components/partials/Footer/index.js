3808bad91a6317205aaf1c4fa72ad1119f2991f0
Gabriel Martins Padoin <gabrielmpwork@gmail.com>
2020-08-23 03:09:57 -0300

    Rodapé (OLX CLONE)

--- olxclone/frontend/src/components/partials/Footer/index.js
+++ olxclone/frontend/src/components/partials/Footer/index.js
@@ -4,7 +4,8 @@ import { FooterArea } from './styled';
 const Footer = () => {
     return (
         <FooterArea>
-            ...
+            <p>Todos os direitos reservados.</p>
+            <p>OLX CLONE B7WEB - POR GABRIEL MARTINS PADOIN</p>
         </FooterArea>
     );
 }
--- olxclone/frontend/src/components/partials/Footer/styled.js
+++ olxclone/frontend/src/components/partials/Footer/styled.js
@@ -3,4 +3,18 @@ import styled from 'styled-components';
 export const FooterArea = styled.div`
     height:100px;
     background-color:#CCC;
+    display:flex;
+    flex-direction:column;
+    justify-content:center;
+    align-items:center;
+    color:#999;
+    font-size:14px;
+    border-top:1px solid #CCC;
+    margin-top:20px;
+
+    p{
+        margin:0;
+        padding:0;
+    }
+
 `;
